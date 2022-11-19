6837a4571e8c9cf5f1e0ca556fb81f772ca1ecb6
Grzesiek <gzaber@tlen.pl>
2022-11-19 18:51:58 +0100

    initial commit

---
+++ packages/timers_api/lib/timers_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library timers_api;
+
+export 'src/timers_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
