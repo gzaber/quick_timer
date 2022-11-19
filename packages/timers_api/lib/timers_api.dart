2ecf379ded7af9b6a4a69bbfc6c594e24aae5469
Grzesiek <gzaber@tlen.pl>
2022-11-19 19:00:20 +0100

    prepare package for developing features

--- packages/timers_api/lib/timers_api.dart
+++ packages/timers_api/lib/timers_api.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library timers_api;
 
-export 'src/timers_api_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/timers_api.dart';
