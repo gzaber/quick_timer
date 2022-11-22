0830bd7e5751223d76448fa2da86cb42cef153e6
Grzesiek <gzaber@tlen.pl>
2022-11-22 19:31:30 +0100

    develop repository

--- packages/timers_repository/lib/src/timers_repository.dart
+++ packages/timers_repository/lib/src/timers_repository.dart
@@ -4,4 +4,40 @@ class TimersRepository {
   TimersRepository(this._timersApi);
 
   final TimersApi _timersApi;
+
+  Future<void> createName(String name) async {
+    await _timersApi.createName(name);
+  }
+
+  Future<void> deleteName(String id) async {
+    _timersApi.deleteName(id);
+  }
+
+  Future<List<Name>> readNames() async {
+    return await _timersApi.readNames();
+  }
+
+  Future<void> createInterval(int minutes) async {
+    await _timersApi.createInterval(minutes);
+  }
+
+  Future<void> deleteInterval(String id) async {
+    await _timersApi.deleteInterval(id);
+  }
+
+  Future<List<Interval>> readIntervals() async {
+    return await _timersApi.readIntervals();
+  }
+
+  Future<void> createTimer(Interval interval, Name name) async {
+    await _timersApi.createTimer(interval, name);
+  }
+
+  Future<void> deleteTimer(String id) async {
+    await _timersApi.deleteTimer(id);
+  }
+
+  Future<List<Timer>> readTimers() async {
+    return await _timersApi.readTimers();
+  }
 }
