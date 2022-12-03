d4536c7f930289a702fcba7526dfcd0d577252c5
Grzesiek <gzaber@tlen.pl>
2022-12-03 19:50:39 +0100

    develop new_timer

--- lib/new_timer/view/new_timer_page.dart
+++ lib/new_timer/view/new_timer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timers_repository/timers_repository.dart' as repo;
@@ -9,7 +11,7 @@ class NewTimerPage extends StatelessWidget {
 
   static Route route() {
     return MaterialPageRoute(
-      builder: (context) => const NewTimerPage(),
+      builder: (_) => const NewTimerPage(),
       settings: const RouteSettings(name: '/new_timer'),
     );
   }
@@ -17,7 +19,7 @@ class NewTimerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => NewTimerBloc(
+      create: (_) => NewTimerBloc(
         timersRepository: RepositoryProvider.of<repo.TimersRepository>(context),
       )..add(NewTimerLoadDataRequested()),
       child: const NewTimerView(),
@@ -35,27 +37,52 @@ class NewTimerView extends StatelessWidget {
         title: const Text('New Timer'),
         toolbarHeight: 107,
         leading: IconButton(
-          onPressed: () => Navigator.pop(context),
+          onPressed: () => Navigator.pop<void>(context),
           icon: const Icon(Icons.arrow_back_ios),
           splashRadius: 25,
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: SizedBox(
-          width: MediaQuery.of(context).size.width - 70,
-          child: const Center(
-            child: Text(
-              'Add to timer',
-              style: TextStyle(fontSize: 16),
+      floatingActionButton: BlocConsumer<NewTimerBloc, NewTimerState>(
+        listener: (context, state) {
+          if (state.creationStatus == CreationStatus.success) {
+            Navigator.pop(context);
+          }
+          if (state.creationStatus == CreationStatus.unselected) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(const SnackBar(
+                  content: Text('Both interval and name should be selected')));
+          }
+          if (state.creationStatus == CreationStatus.failure) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                  const SnackBar(content: Text('Something went wrong')));
+          }
+        },
+        builder: (context, state) {
+          return FloatingActionButton.extended(
+            onPressed: () {
+              context.read<NewTimerBloc>().add(NewTimerCreated());
+            },
+            label: SizedBox(
+              width: MediaQuery.of(context).size.width - 70,
+              child: Center(
+                child: state.creationStatus == CreationStatus.loading
+                    ? const CircularProgressIndicator()
+                    : const Text(
+                        'Add to timer',
+                        style: TextStyle(fontSize: 16),
+                      ),
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const _HeaderText(title: 'Select time'),
             Container(
@@ -121,68 +148,137 @@ class _Intervals extends StatelessWidget {
       itemCount: intervals.length + 1,
       itemBuilder: (context, index) {
         if (index == intervals.length) {
-          return Column(
-            children: [
-              const SizedBox(height: 15),
-              IconButton(
-                onPressed: () {
-                  _CreateIntervalDialog.show(context, title: 'Select minutes')
-                      .then(
-                    (value) {
-                      if (value != null) {
-                        context
-                            .read<NewTimerBloc>()
-                            .add(NewTimerIntervalAdded(minutes: value));
-                      }
-                    },
-                  );
-                },
-                icon: const Icon(
-                  Icons.add,
-                  size: 30,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          );
+          return const _CreateIntervalButton();
         } else {
-          return GestureDetector(
-            onLongPress: () {
-              _DeleteDialog.show(context, itemName: 'interval').then((value) {
-                if (value == true) {
-                  context
-                      .read<NewTimerBloc>()
-                      .add(NewTimerIntervalDeleted(interval: intervals[index]));
-                }
-              });
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 75,
-                    height: 75,
-                    decoration: const BoxDecoration(
-                      color: Colors.pink,
-                      shape: BoxShape.circle,
-                    ),
-                    margin: const EdgeInsets.only(bottom: 10),
-                  ),
-                  Text(
-                    '${intervals[index].minutes} min',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
+          return _IntervalItem(interval: intervals[index]);
         }
       },
+    );
+  }
+}
+
+class _IntervalItem extends StatelessWidget {
+  const _IntervalItem({
+    Key? key,
+    required this.interval,
+  }) : super(key: key);
+
+  final repo.Interval interval;
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedInterval =
+        context.select((NewTimerBloc bloc) => bloc.state.selectedInterval);
+
+    return GestureDetector(
+      onTap: () {
+        context
+            .read<NewTimerBloc>()
+            .add(NewTimerIntervalSelected(interval: interval));
+      },
+      onLongPress: () {
+        _DeleteDialog.show(context, itemName: 'interval').then((value) {
+          if (value == true) {
+            context
+                .read<NewTimerBloc>()
+                .add(NewTimerIntervalDeleted(interval: interval));
+          }
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: 75,
+                  height: 75,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF111B3E),
+                    shape: BoxShape.circle,
+                  ),
+                  margin: const EdgeInsets.only(bottom: 10),
+                ),
+                CustomPaint(
+                  painter: _CircleSectorPainter(
+                    minutes: interval.minutes,
+                    color: selectedInterval == interval
+                        ? const Color(0xFFFF82A1)
+                        : const Color(0xFF233266),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              '${interval.minutes} min',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CircleSectorPainter extends CustomPainter {
+  _CircleSectorPainter({
+    required this.minutes,
+    required this.color,
+  });
+
+  final int minutes;
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    const rect = Rect.fromLTRB(0, 0, 75, 75);
+    const startAngle = -math.pi / 2;
+    final sweepAngle = 2 * math.pi * minutes / 60;
+    const useCenter = true;
+    final paint = Paint()..color = color;
+    canvas.drawArc(rect, startAngle, sweepAngle, useCenter, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class _CreateIntervalButton extends StatelessWidget {
+  const _CreateIntervalButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 15),
+        IconButton(
+          onPressed: () {
+            _CreateIntervalDialog.show(context, title: 'Select minutes').then(
+              (value) {
+                if (value != null) {
+                  context
+                      .read<NewTimerBloc>()
+                      .add(NewTimerIntervalCreated(minutes: value));
+                }
+              },
+            );
+          },
+          icon: const Icon(
+            Icons.add,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
@@ -198,51 +294,96 @@ class _Names extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       child: Wrap(
+        spacing: 10,
+        runSpacing: 15,
         children: List.generate(
           names.length + 1,
           (index) {
             if (index == names.length) {
-              return IconButton(
-                onPressed: () {
-                  _CreateNameDialog.show(context, title: 'Create name').then(
-                    (value) {
-                      if (value != null) {
-                        context
-                            .read<NewTimerBloc>()
-                            .add(NewTimerNameAdded(name: value));
-                      }
-                    },
-                  );
-                },
-                icon: const Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-              );
+              return const _CreateNameButton();
             } else {
-              return GestureDetector(
-                onLongPress: () {
-                  _DeleteDialog.show(context, itemName: 'name').then((value) {
-                    if (value == true) {
-                      context
-                          .read<NewTimerBloc>()
-                          .add(NewTimerNameDeleted(name: names[index]));
-                    }
-                  });
-                },
-                child: Container(
-                  color: Colors.amber,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  margin: const EdgeInsets.only(right: 10, top: 10),
-                  child: Text(names[index].name),
-                ),
-              );
+              return _NameItem(name: names[index]);
             }
           },
         ),
+      ),
+    );
+  }
+}
+
+class _NameItem extends StatelessWidget {
+  const _NameItem({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  final repo.Name name;
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedName =
+        context.select((NewTimerBloc bloc) => bloc.state.selectedName);
+
+    return GestureDetector(
+      onTap: () {
+        context.read<NewTimerBloc>().add(NewTimerNameSelected(name: name));
+      },
+      onLongPress: () {
+        _DeleteDialog.show(context, itemName: 'name').then((value) {
+          if (value == true) {
+            context.read<NewTimerBloc>().add(NewTimerNameDeleted(name: name));
+          }
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          color: name == selectedName
+              ? const Color(0xFFFF82A1)
+              : const Color(0xFF343D58),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          name.name,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CreateNameButton extends StatelessWidget {
+  const _CreateNameButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 42,
+      child: IconButton(
+        onPressed: () {
+          _CreateNameDialog.show(context, title: 'Create name').then(
+            (value) {
+              if (value != null) {
+                context
+                    .read<NewTimerBloc>()
+                    .add(NewTimerNameCreated(name: value));
+              }
+            },
+          );
+        },
+        icon: const Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
+        ),
+        splashRadius: 25,
       ),
     );
   }
