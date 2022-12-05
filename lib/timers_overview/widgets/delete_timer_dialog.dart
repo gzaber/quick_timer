e98ea574cddcc4d61ea74adb066deda7cf52fdcf
Grzesiek <gzaber@tlen.pl>
2022-12-05 20:35:22 +0100

    move widgets to separate folder

---
+++ lib/timers_overview/widgets/delete_timer_dialog.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class DeleteTimerDialog extends StatelessWidget {
+  const DeleteTimerDialog({
+    Key? key,
+    required this.itemName,
+  }) : super(key: key);
+
+  final String itemName;
+
+  static Future<bool?> show(BuildContext context, {required String itemName}) {
+    return showDialog<bool>(
+      context: context,
+      useRootNavigator: false,
+      builder: (_) => DeleteTimerDialog(itemName: itemName),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Delete'),
+      content: Text('Delete $itemName?'),
+      actionsAlignment: MainAxisAlignment.spaceBetween,
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop<bool>(context, false),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pop<bool>(context, true),
+          child: const Text('OK'),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/timers_overview/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'delete_timer_dialog.dart';
+export 'header_text.dart';
