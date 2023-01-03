0530fad08bdbdfaee2d2a3b9a36ff5cdb3bf8097
Grzesiek <gzaber@tlen.pl>
2023-01-03 20:01:30 +0100

    update Timer model and TimersApi to count startup

--- packages/timers_api/lib/src/timers_api.dart
+++ packages/timers_api/lib/src/timers_api.dart
@@ -1,13 +1,14 @@
 import 'models/models.dart';
 
 abstract class TimersApi {
-  Future<void> createName(String name);
-  Future<void> deleteName(String id);
-  Future<List<Name>> readNames();
   Future<void> createInterval(int minutes);
   Future<void> deleteInterval(String id);
   Future<List<Interval>> readIntervals();
+  Future<void> createName(String name);
+  Future<void> deleteName(String id);
+  Future<List<Name>> readNames();
   Future<void> createTimer(Interval interval, Name name);
   Future<void> deleteTimer(String id);
   Future<List<Timer>> readTimers();
+  Future<void> incrementStartupCounter(Timer timer);
 }
--- packages/timers_api/test/models/timer_test.dart
+++ packages/timers_api/test/models/timer_test.dart
@@ -4,6 +4,7 @@ import 'package:timers_api/src/models/models.dart';
 void main() {
   group('Timer', () {
     final id = 'id';
+    final startupCounter = 0;
     final name = Name(id: 'nameId', name: 'name');
     final interval = Interval(id: 'intervalId', minutes: 10);
 
@@ -16,6 +17,13 @@ void main() {
         expect(
           Timer(interval: interval, name: name).id,
           isNotEmpty,
+        );
+      });
+
+      test('sets startupCounter to 0 if not provided', () {
+        expect(
+          Timer(interval: interval, name: name).startupCounter,
+          equals(0),
         );
       });
     });
@@ -32,6 +40,7 @@ void main() {
           Timer(id: id, interval: interval, name: name).props,
           equals([
             'id',
+            0,
             Name(id: 'nameId', name: 'name'),
             Interval(id: 'intervalId', minutes: 10),
           ]));
@@ -42,6 +51,7 @@ void main() {
         expect(
           Timer.fromJson({
             "id": id,
+            "startupCounter": startupCounter,
             "interval": {
               "id": "intervalId",
               "minutes": 10,
@@ -51,7 +61,11 @@ void main() {
               "name": "name",
             }
           }),
-          equals(Timer(id: id, interval: interval, name: name)),
+          equals(Timer(
+              id: id,
+              startupCounter: startupCounter,
+              interval: interval,
+              name: name)),
         );
       });
     });
@@ -62,6 +76,7 @@ void main() {
           Timer(id: id, interval: interval, name: name).toJson(),
           equals({
             "id": id,
+            "startupCounter": startupCounter,
             "interval": {
               "id": "intervalId",
               "minutes": 10,
@@ -71,6 +86,43 @@ void main() {
               "name": "name",
             }
           }),
+        );
+      });
+    });
+
+    group('copyWith', () {
+      final timer = Timer(id: id, interval: interval, name: name);
+
+      test('return the same object if no arguments are provided', () {
+        expect(
+          timer.copyWith(),
+          equals(timer),
+        );
+      });
+
+      test('retains the old value for every null parameter', () {
+        expect(
+          timer.copyWith(
+              id: null, startupCounter: null, interval: null, name: null),
+          equals(timer),
+        );
+      });
+
+      test('replaces non null parameters', () {
+        final newInterval = Interval(id: 'newIntervalId', minutes: 6);
+        final newName = Name(id: 'newNameId', name: 'newName');
+
+        expect(
+          timer.copyWith(
+              id: 'idd',
+              startupCounter: 6,
+              interval: newInterval,
+              name: newName),
+          equals(Timer(
+              id: 'idd',
+              startupCounter: 6,
+              interval: newInterval,
+              name: newName)),
         );
       });
     });
